722aefe4a8ca8828543ac860fd33bc9e51e083c9
mechellepd <mechelle.begal@niat.com.bn>
2026-09-10 09:52:02 +0800

    Changes for maritime page

--- src/views/MaritimeView.vue
+++ src/views/MaritimeView.vue
@@ -17,7 +17,7 @@
         <p>
           Comprehensive maritime surveillance and intelligence through satellite observation,
           supporting navigation safety, resource management, security operations, and environmental
-          protection across maritime domains. Tesgting for example.
+          protection across maritime domains. Changes if for testing only.
         </p>
       </div>
     </section>
