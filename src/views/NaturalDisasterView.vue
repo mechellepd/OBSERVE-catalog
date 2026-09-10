b8fa8b1fb73d61c9b237412745cd41024b44749b
mechellepd <mechelle.begal@niat.com.bn>
2026-09-10 13:51:57 +0800

    changes

--- src/views/NaturalDisasterView.vue
+++ src/views/NaturalDisasterView.vue
@@ -18,7 +18,7 @@
         <p>
           Satellite observation provides critical data for disaster preparedness, response, and
           recovery, enabling rapid assessment and coordinated relief efforts for communities at
-          risk.
+          risk. Testing.
         </p>
       </div>
     </section>
